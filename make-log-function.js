2b49ae8c090ad804c39d4a5c789e02c040c9d248
Matthew Rafferty <mmraff@gmail.com>
2015-10-02 17:19:54 -0600

    initial commit

---
+++ make-log-function.js
@@ -0,0 +1,50 @@
+module.exports = makeLogFunc
+
+var assert = require("assert")
+  ;
+
+var levelMap = {
+        ERROR: 3
+      , WARN: 2
+      , INFO: 1
+      , VERBOSE: 0
+    }
+  ;
+function makeLogFunc (minLevel)
+{
+  assert(minLevel && typeof minLevel == "string",
+         "Must give minimum debug level as a string")
+  var MINLEVEL = minLevel.toUpperCase()
+  assert(MINLEVEL in levelMap, "Can't makeLogFunction with "+minLevel)
+
+  return function (lvl, msg) {
+    assert(typeof msg != "undefined",
+           "Must give message/value to display")
+    assert(typeof lvl == "string" && 0 < lvl.length,
+           "debug level must be a non-empty string")
+
+    var LVL = lvl.toUpperCase()
+    assert(LVL in levelMap, "unrecognized debug level '"+lvl+"'")
+
+    if (levelMap[LVL] < levelMap[MINLEVEL]) { return }
+
+    var fmt = (typeof msg == "number" ? "%d" :
+              (typeof msg == "object" ? "%j" : "%s"))
+
+    switch (LVL) {
+      case "ERROR":
+        console.error("\u001b[31mERR!\u001b[0m "+fmt, msg)
+        break
+      case "WARN":
+        console.warn("\u001b[33mWARN\u001b[0m "+fmt, msg)
+        break
+      case "INFO":
+        console.info("\u001b[32minfo\u001b[0m "+fmt, msg)
+        break
+      case "VERBOSE":
+        console.info("\u001b[34mverb\u001b[0m "+fmt, msg)
+        break
+    }
+  }
+}
+
